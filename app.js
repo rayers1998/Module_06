7e2995c048048b53d89ff812c16084c4a14ff3b4
Rene Ayers <rayers1998@gmail.com>
2024-07-25 14:54:40 -0400

    added multer to to parse multipart/form-data in middleware.

--- app.js
+++ app.js
@@ -1,21 +1,78 @@
+// // app.js
+
+// require('dotenv').config();   // Load environment variables from a .env file
+// const express = require('express');  // Import Express framework
+// const cors = require('cors'); // Import CORS to allow cross-origin requests
+// //const chalk = require('chalk');  // Import Chalk for colored console logs
+// const app = express();  // Create an Express application
+// const port = process.env.PORT || 3004; // Define the port to run the server on
+
+
+// // Import the MongoDB manager to handle database connections
+// const MongoManager = require('./src/shared/db/mongodb/mongo-manager');
+
+
+// app.use(cors()); // Use CORS to handle cross-origin requests
+// app.use(express.static('./src/public'));  // Serve static files from the 'public' folder
+// app.use(express.urlencoded({ extended: true }));  // Parse URL-encoded data from forms
+// app.use(express.json());  // Parse JSON data in requests
+
+// // Import and register middleware
+// const middleWare = require('./src/shared/middleware/base-middleware');
+// middleWare.registerBaseMiddleWare(app);
+
+// // Function to set up routes
+
+//     // Import route handlers
+//     const loginRoutes = require('./src/routes/login.routes');
+//     const healthRoutes = require('./src/routes/health.routes');
+//     const adminRoutes = require('./src/routes/admin.routes');
+//     const publicRoutes = require('./src/routes/public.routes');
+//     const regionRouter = require('./src/routes/region.routes');
+//     const agentRoutes = require('./src/routes/agent.routes');
+
+//     // Applying route prefixes to structure API endpoints for improved clarity and organization
+//     app.use('/api/login', loginRoutes);
+//     healthRoutes.registerHealthRoutes(app);
+//     adminRoutes.registerAdminRoutes(app);
+//     publicRoutes.registerPublicRoutes(app);
+//     regionRouter.registerRegionRoutes(app);
+//     app.use('/api', agentRoutes);
+
+
+
+// // Connect to Mongodb
+// MongoManager.openMongoConnection();
+
+// // Start the server and listen for requests on the specified port
+// app.listen(port, () => {
+//     console.log(`Server is listening on port ${port}`);
+// });
+
+// registerBaseMiddleWare function from your middleware file is called to apply the base middleware to the Express app.
+
 // app.js
 
 require('dotenv').config();   // Load environment variables from a .env file
 const express = require('express');  // Import Express framework
 const cors = require('cors'); // Import CORS to allow cross-origin requests
-//const chalk = require('chalk');  // Import Chalk for colored console logs
 const app = express();  // Create an Express application
 const port = process.env.PORT || 3004; // Define the port to run the server on
-
 
 // Import the MongoDB manager to handle database connections
 const MongoManager = require('./src/shared/db/mongodb/mongo-manager');
 
+// Use CORS to handle cross-origin requests
+app.use(cors());
 
-app.use(cors()); // Use CORS to handle cross-origin requests
-app.use(express.static('./src/public'));  // Serve static files from the 'public' folder
-app.use(express.urlencoded({ extended: true }));  // Parse URL-encoded data from forms
-app.use(express.json());  // Parse JSON data in requests
+// Serve static files from the 'public' folder
+app.use(express.static('./src/public'));
+
+// Parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: true }));
+
+// Parse JSON data in requests
+app.use(express.json());
 
 // Import and register middleware
 const middleWare = require('./src/shared/middleware/base-middleware');
@@ -23,30 +80,27 @@ middleWare.registerBaseMiddleWare(app);
 
 // Function to set up routes
 
-    // Import route handlers
-    const loginRoutes = require('./src/routes/login.routes');
-    const healthRoutes = require('./src/routes/health.routes');
-    const adminRoutes = require('./src/routes/admin.routes');
-    const publicRoutes = require('./src/routes/public.routes');
-    const regionRouter = require('./src/routes/region.routes');
-    const agentRoutes = require('./src/routes/agent.routes');
+// Import route handlers
+const loginRoutes = require('./src/routes/login.routes');
+const healthRoutes = require('./src/routes/health.routes');
+const adminRoutes = require('./src/routes/admin.routes');
+const publicRoutes = require('./src/routes/public.routes');
+const regionRouter = require('./src/routes/region.routes');
+const agentRoutes = require('./src/routes/agent.routes');
 
-    // Applying route prefixes to structure API endpoints for improved clarity and organization
-    app.use('/api/login', loginRoutes);
-    healthRoutes.registerHealthRoutes(app);
-    adminRoutes.registerAdminRoutes(app);
-    publicRoutes.registerPublicRoutes(app);
-    regionRouter.registerRegionRoutes(app);
-    app.use('/api', agentRoutes);
+// Applying route prefixes to structure API endpoints for improved clarity and organization
+app.use('/api/login', loginRoutes);
+healthRoutes.registerHealthRoutes(app);
+adminRoutes.registerAdminRoutes(app);
+publicRoutes.registerPublicRoutes(app);
+regionRouter.registerRegionRoutes(app);
+app.use('/api', agentRoutes);
 
-
-
-// Connect to Mongodb
+// Connect to MongoDB
 MongoManager.openMongoConnection();
 
 // Start the server and listen for requests on the specified port
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
-
 
--- src/shared/middleware/base-middleware.js
+++ src/shared/middleware/base-middleware.js
@@ -5,12 +5,94 @@
 */
 
 
-// src/shared/middleware/base-middleware.js
+// // src/shared/middleware/base-middleware.js
 
+
+// require('dotenv').config();  // Load environment variables from a .env file
+// const Express = require('express');  // Import the Express library
+// //const chalk = require('chalk'); // Import the Chalk library for colored console logs
+// const jwt = require('jsonwebtoken'); // Import the JSON Web Token library for handling JWTs
+
+// // List of admin routes that require special handling
+// const adminRoutes = [
+//   '/email-list',
+//   '/region-avg',
+//   '/calc'
+// ];
+
+// // Middleware for logging API calls with colors
+// // /const coloredLogger = (req, res, next) => {  
+// //   const timestamp = chalk.blue(new Date().toLocaleString('en-CA', { timeZone: 'America/New_York' })); // Create a timestamp with a specific format and time zone
+// //   const method = req.method; // Get the HTTP method of the request (GET, POST, etc.)
+// //   const methodColor = method === 'GET' ? chalk.green :  // Choose a color based on the HTTP method
+// //                       method === 'POST' ? chalk.blue :
+// //                       method === 'DELETE' ? chalk.red : chalk.yellow;
+// //   const methodStyled = methodColor.bold(method);  // Style the method text with the chosen color
+// //   const urlStyled = chalk.cyan(req.originalUrl);  // Style the URL text
+// //   const timezone = 'Eastern Daylight Time'; // Define the time zone for logging
+
+// //   console.log(`API call: ${methodStyled} ${urlStyled} at ${timestamp} [${timezone}]`);  // Print the log message with colored method and URL
+// //   next();  // Move on to the next middleware function
+// // };
+
+// // Middleware to check JWT token for specific routes
+// const checkAuthToken = (req, res, next) => {
+//   // Adjust this list to include routes that require JWT authentication
+//   const jwtProtectedRoutes = [
+//     '/api/agent-create', 
+//     '/api/region', 
+//     /* '/api/agents', Uncomment to protect the agents route */
+//     '/api/agent-update-info', 
+//     '/api/agent-delete', 
+//     '/region-create', 
+//     '/all-stars',
+//     '/region-avg',
+//     '/region',
+//     '/api/agents-by-region',
+//     '/email-list'
+//   ];
+
+//   // Get the route (base URL), and remove query parameters if present
+//   const baseUrl = req.url.split('?')[0];
+
+  
+//   if (jwtProtectedRoutes.some(protectedPath => baseUrl.startsWith(protectedPath))) {  // Check if the current route requires JWT authentication
+//     const authHeader = req.headers.authorization; // Get the Authorization header from the request
+    
+//     if (authHeader && authHeader.startsWith('Bearer ')) { // Check if the header exists and starts with 'Bearer '
+//       const token = authHeader.substring(7); // Extract the token from the header
+    
+//       if(token==process.env.JWT_SECRET){next}
+//       // jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {   //  Verify the token using the secret key  
+//       //     if (err) { 
+//       //     return res.status(403).json({ message: 'Forbidden: Invalid token' }); // If there's an error verifying the token, respond with Forbidden
+//       //   }
+//       //   req.user = decoded; // If the token is valid, attach the decoded user info to the request
+//       //   next();  // Move on to the next middleware function
+//       // });
+//     } else {
+//         return res.status(401).json({ message: 'Authorization header is required and must be in the format Bearer <token>' });  // If the Authorization header is missing or invalid, respond with Unauthorized
+//     }
+//   } else {
+//     next();  // If the route doesn't need JWT authentication, move on to the next middleware function
+//   }
+// };
+
+// // Function to register the base middleware for the app
+// const registerBaseMiddleWare = (app) => {
+  
+//   app.use(Express.json());  // Parse JSON bodies in incoming requests
+//   // app.use(coloredLogger);  // Apply the colored logging middleware
+//   app.use(checkAuthToken); // Apply the JWT check middleware
+// };
+
+// // Export the function to register base middleware
+// module.exports = { registerBaseMiddleWare };
+
+// adding Multer
 
 require('dotenv').config();  // Load environment variables from a .env file
 const Express = require('express');  // Import the Express library
-//const chalk = require('chalk'); // Import the Chalk library for colored console logs
 const jwt = require('jsonwebtoken'); // Import the JSON Web Token library for handling JWTs
 
 // List of admin routes that require special handling
@@ -19,21 +101,6 @@ const adminRoutes = [
   '/region-avg',
   '/calc'
 ];
-
-// Middleware for logging API calls with colors
-// /const coloredLogger = (req, res, next) => {  
-//   const timestamp = chalk.blue(new Date().toLocaleString('en-CA', { timeZone: 'America/New_York' })); // Create a timestamp with a specific format and time zone
-//   const method = req.method; // Get the HTTP method of the request (GET, POST, etc.)
-//   const methodColor = method === 'GET' ? chalk.green :  // Choose a color based on the HTTP method
-//                       method === 'POST' ? chalk.blue :
-//                       method === 'DELETE' ? chalk.red : chalk.yellow;
-//   const methodStyled = methodColor.bold(method);  // Style the method text with the chosen color
-//   const urlStyled = chalk.cyan(req.originalUrl);  // Style the URL text
-//   const timezone = 'Eastern Daylight Time'; // Define the time zone for logging
-
-//   console.log(`API call: ${methodStyled} ${urlStyled} at ${timestamp} [${timezone}]`);  // Print the log message with colored method and URL
-//   next();  // Move on to the next middleware function
-// };
 
 // Middleware to check JWT token for specific routes
 const checkAuthToken = (req, res, next) => {
@@ -55,23 +122,21 @@ const checkAuthToken = (req, res, next) => {
   // Get the route (base URL), and remove query parameters if present
   const baseUrl = req.url.split('?')[0];
 
-  
   if (jwtProtectedRoutes.some(protectedPath => baseUrl.startsWith(protectedPath))) {  // Check if the current route requires JWT authentication
     const authHeader = req.headers.authorization; // Get the Authorization header from the request
     
     if (authHeader && authHeader.startsWith('Bearer ')) { // Check if the header exists and starts with 'Bearer '
       const token = authHeader.substring(7); // Extract the token from the header
     
-      if(token==process.env.JWT_SECRET){next}
-      // jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {   //  Verify the token using the secret key  
-      //     if (err) { 
-      //     return res.status(403).json({ message: 'Forbidden: Invalid token' }); // If there's an error verifying the token, respond with Forbidden
-      //   }
-      //   req.user = decoded; // If the token is valid, attach the decoded user info to the request
-      //   next();  // Move on to the next middleware function
-      // });
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {   //  Verify the token using the secret key  
+        if (err) { 
+          return res.status(403).json({ message: 'Forbidden: Invalid token' }); // If there's an error verifying the token, respond with Forbidden
+        }
+        req.user = decoded; // If the token is valid, attach the decoded user info to the request
+        next();  // Move on to the next middleware function
+      });
     } else {
-        return res.status(401).json({ message: 'Authorization header is required and must be in the format Bearer <token>' });  // If the Authorization header is missing or invalid, respond with Unauthorized
+      return res.status(401).json({ message: 'Authorization header is required and must be in the format Bearer <token>' });  // If the Authorization header is missing or invalid, respond with Unauthorized
     }
   } else {
     next();  // If the route doesn't need JWT authentication, move on to the next middleware function
@@ -80,11 +145,10 @@ const checkAuthToken = (req, res, next) => {
 
 // Function to register the base middleware for the app
 const registerBaseMiddleWare = (app) => {
-  
   app.use(Express.json());  // Parse JSON bodies in incoming requests
-  // app.use(coloredLogger);  // Apply the colored logging middleware
   app.use(checkAuthToken); // Apply the JWT check middleware
 };
 
 // Export the function to register base middleware
 module.exports = { registerBaseMiddleWare };
+
